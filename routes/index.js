2f95fe1a3b8d717ac0fba6362359082bba060834
JohnIrle <irlejohn@gmail.com>
2018-06-23 11:50:07 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
-router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
-router.get('/favicon.ico', function(req, res) {
-    res.status(204);
+router.get("/favicon.ico", function(req, res) {
+  res.status(204);
 });
 
-router.get('/api/whoami', function(req, res) {
-  const ip = req.get('x-forwarded-for');
-  const ipParse = ip.split(',');
-  const language = req.get('accept-language');
-  const languageParse = language.split(',');
-  const software = req.get('user-agent');
+router.get("/api/whoami", function(req, res) {
+  const ip = req.get("x-forwarded-for");
+  const ipParse = ip.split(",");
+  const language = req.get("accept-language");
+  const languageParse = language.split(",");
+  const software = req.get("user-agent");
   const softwareParse = software.split(/[()]+/);
 
   res.send({
@@ -24,7 +24,7 @@ router.get('/api/whoami', function(req, res) {
     software: softwareParse[1]
   });
 
-  console.log(req.headers)
+  console.log(req.headers);
 });
 
 module.exports = router;
