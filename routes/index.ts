09a84fba6f8290722f4ea77491c617fd90687cf0
John Irle <irlejohn@gmail.com>
2021-11-24 23:49:28 -0600

    refactor: change file to ts

---
+++ routes/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+const router = express.Router();
+import path from "path";
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+
+router.get("/favicon.ico", function (req, res) {
+  res.status(204);
+});
+
+router.get("/api/whoami", function (req, res) {
+  const ip = req.get("x-forwarded-for");
+  const ipParse = ip?.split(",");
+  const language = req.get("accept-language");
+  const languageParse = language?.split(",");
+  const software = req.get("user-agent");
+  const softwareParse = software?.split(/[()]+/);
+
+  res.send({
+    ipaddress: ipParse ? ipParse[0] : "no ip available",
+    language: languageParse ? languageParse[0] : "no language available",
+    software: softwareParse ? softwareParse[1] : "no software available",
+  });
+});
+
+export default router;
